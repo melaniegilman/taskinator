de86aa0ad23047d57dd9ddeb657213a9aa31807b
Melanie Gilman <melaniegilman259@yahoo.com>
2020-06-25 14:27:50 -0400

    added backgrounds when dragging

--- assets/js/script.js
+++ assets/js/script.js
@@ -187,7 +187,7 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
       event.preventDefault();
-      
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
 };
  
@@ -207,7 +207,14 @@ var dropTaskHandler = function(event) {
       else if (statusType === "tasks-completed") {
         statusSelectEl.selectedIndex = 2;
       }
+      dropZoneEl.removeAttribute("style");
       dropZoneEl.appendChild(draggableElement);
+  };
+  var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+    taskListEl.removeAttribute("style");
+}
   };
   //Other Logic
   pageContentEl.addEventListener("click", taskButtonHandler);
@@ -215,3 +222,4 @@ var dropTaskHandler = function(event) {
   pageContentEl.addEventListener("dragstart", dragTaskHandler);
   pageContentEl.addEventListener("dragover", dropZoneDragHandler);
   pageContentEl.addEventListener("drop", dropTaskHandler);
+  pageContentEl.addEventListener("dragleave", dragLeaveHandler);
